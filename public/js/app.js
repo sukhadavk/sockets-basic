cd883c5bfbbb6d17c5bb81f4d46ba85558f6f4c8
Sukhada Karhade <sukhada.karhade@sigma-systems.com>
2016-02-15 20:29:45 +0530

    chat app

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,24 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('connected to socket io server');
 });
+
+socket.on('message', function (message) {
+	console.log('Resecived message : ');
+	console.log(message.text);
+
+	jQuery('.messages').append('<p>' + message.text + '</p>');
+});
+
+//handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+	var $message = $form.find('input[name=message]');
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	//clean the message
+	$message.val('');
+});
--- server.js
+++ server.js
@@ -3,14 +3,26 @@ var express = require('express');
 var app = express();
 
 var http = require('http').Server(app);
-var io =  require('socket.io')(http);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function () {
+io.on('connection', function(socket) {
 	console.log('user connected via socket.io');
+
+	socket.on('message', function(message) {
+		console.log('Message Received : ' + message.text);
+		//io.emit -  vereyone including sender
+		io.emit('message', message);
+		//excluding sender
+		//socket.broadcast.emit('message', message);
+	});
+
+	socket.emit('message', {
+		text: 'Welcome to chat application!'
+	});
 });
 
-http.listen(PORT, function () {
+http.listen(PORT, function() {
 	console.log('Server started')
 });
