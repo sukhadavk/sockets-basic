ecadf116fa404e82664905f6920a5c895f079b2a
Sukhada Karhade <sukhada.karhade@sigma-systems.com>
2016-02-16 11:28:58 +0530

    aded moment to chat appl

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,23 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
 
+console.log(name + ' wants to join '+ room);
+
 socket.on('connect', function () {
-	console.log('connected to socket io server');
+	console.log('connected to socket io server');	
 });
 
 socket.on('message', function (message) {
 	var momentTimestamp =  moment.utc(message.tomestamp);
+	var $message = jQuery('.messages');
 	console.log('Resecived message : ');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p><strong>' +  momentTimestamp.local().format('h:mm a') + '</strong>  ' + message.text + '</p>');
+	$message.append('<p><strong> ' + message.name + ' ' +  momentTimestamp.local().format('h:mm a') + '</strong> </p> ');;
+	$message.append('<p>'+ message.text + '</p>');
 });
+	
 
 //handles submitting of new message
 var $form = jQuery('#message-form');
@@ -19,6 +26,7 @@ $form.on('submit', function (event) {
 	event.preventDefault();
 	var $message = $form.find('input[name=message]');
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 
