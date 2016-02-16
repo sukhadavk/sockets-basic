53f1a7f0bea757e23e60d5c2794040c6a7eab7fb
Sukhada Karhade <sukhada.karhade@sigma-systems.com>
2016-02-16 14:14:41 +0530

    aded moment to chat appl

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
+//send current users to provided sockets
+function sendCurrentUsers(socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if(typeof info === 'undefined'){
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+		if(userInfo.room === info.room) {
+			users.push(userInfo.name);
+		}
+	});
+	
+	socket.emit('message', {
+				name: 'System',
+				text: 'Current users: ' + users.join(', '),
+				timestamp: moment().valueOf()		
+	});
+};
+
 
 io.on('connection', function(socket) {
 	console.log('user connected via socket.io');
@@ -39,11 +62,12 @@ io.on('connection', function(socket) {
 	socket.on('message', function(message) {
 		console.log('Message Received : ' + message.text);
 
-		message.timestamp = moment().valueOf();
-		//io.emit -  vereyone including sender
-		io.to(clientInfo[socket.id].room).emit('message', message);
-		//excluding sender
-		//socket.broadcast.emit('message', message);
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	});
 
 	socket.emit('message', {
